414c64d6e572db623cfc186c8a3b01c66dc424d2
Bondarenko Anastasia <jumkot@mail.ru>
2023-04-20 23:28:00 +0700

    Start laba3

---
+++ laba3/random.c
@@ -0,0 +1,76 @@
+// https://csc-programming.readthedocs.io/labs/03-bitwise-operations.html
+
+// Разработайте приложение, которое генерирует 1000000 случайных чисел и записывает их в два бинарных файла.
+// В файл uncompressed.dat запишите числа в несжатом формате, в файл compressed.dat — в формате varint.
+// Сравните размеры файлов.
+// Реализуйте чтение чисел из двух файлов.
+// Добавьте проверку: последовательности чисел из двух файлов должны совпадать.
+// Использование формата varint наиболее эффективно в случаях, когда подавляющая доля чисел имеет
+// небольшие значения.
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
+size_t encode_varint(uint32_t value, uint8_t* buf)
+{
+    assert(buf != NULL);
+    uint8_t* cur = buf;
+    while (value >= 0x80) {
+        const uint8_t byte = (value & 0x7f) | 0x80;
+        *cur = byte;
+        value >>= 7;
+        ++cur;
+    }
+    *cur = value;
+    ++cur;
+    return cur - buf;
+}
+
+uint32_t decode_varint(const uint8_t** bufp)
+{
+    const uint8_t* cur = *bufp;
+    uint8_t byte = *cur++;
+    uint32_t value = byte & 0x7f;
+    size_t shift = 7;
+    while (byte >= 0x80) {
+        byte = *cur++;
+        value += (byte & 0x7f) << shift;
+        shift += 7;
+    }
+    *bufp = cur;
+    return value;
+}
+
+/*
+ * Диапазон             Вероятность
+ * -------------------- -----------
+ * [0; 128)             90%
+ * [128; 16384)         5%
+ * [16384; 2097152)     4%
+ * [2097152; 268435455) 1%
+ */
+uint32_t generate_number()
+{
+    const int r = rand();
+    const int p = r % 100;
+    if (p < 90) {
+        return r % 128;
+    }
+    if (p < 95) {
+        return r % 16384;
+    }
+    if (p < 99) {
+        return r % 2097152;
+    }
+    return r % 268435455;
+}
+
+
+int main()
+{
+    FILE* uncompressed = fopen("uncompressed.dat", "wb");
+    FILE* compressed = fopen("compressed.dat", "wb");
+
+    return 0;
+}
