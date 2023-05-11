24033f7631ba1be6ffc70fee59315314612eabd9
Bondarenko Anastasia <jumkot@mail.ru>
2023-05-12 03:12:08 +0700

    Create read_next_code_unit, need to enter in it

--- laba3/2/src/coder.c
+++ laba3/2/src/coder.c
@@ -1,3 +1,6 @@
+#include <assert.h>
+#include <inttypes.h>
+
 int encode(uint32_t code_point, CodeUnit* code_unit)
 {
     assert(code_unit != NULL);
@@ -21,7 +24,7 @@ int encode(uint32_t code_point, CodeUnit* code_unit)
         code_unit->code[2] = 0x80 | ((code_point >> 6) & 0x3f);
         code_unit->code[3] = 0x80 | (code_point & 0x3f);
     } else {
-        printf("%d превышает масимальное значение для кодирования\n", code_point);
+        printf("Ошибка: %d превышает масимальное значение для кодирования\n", code_point);
         code_unit->length = 0;
 
         return -1;
@@ -68,5 +71,82 @@ int write_code_unit(FILE* out, const CodeUnits* code_unit)
 
 int read_next_code_unit(FILE* in, CodeUnits* code_units)
 {
-    
+
+}
+
+
+
+int read_next_code_unit(FILE* in, CodeUnit* code_unit)
+{
+    if (in == NULL) {
+        return -1;
+    }
+
+    if (fread(code_unit->code, 1, 1, in) != 1) {
+        if (feof(in)) {
+            return -1;
+        }
+    }
+
+    while (1) {
+        if (feof(in)) {
+            code_unit = NULL;
+            return -1;
+        }
+        if ((code_unit->code[0] & 0x80) == 0) {
+            code_unit->length = 1;
+
+            return 0;
+        } else if (code_unit->code[0] >> 5 == 0x6) {
+            if (fread(&(code_unit->code[1]), 1, 1, in) != 1)
+                continue;
+            if (code_unit->code[1] >> 6 != 0x2) {
+                code_unit->code[0] = code_unit->code[1];
+                continue;
+            }
+            code_unit->length = 2;
+
+            return 0;
+        } else if (code_unit->code[0] >> 4 == 0xe) {
+            if (fread(&(code_unit->code[1]), 1, 1, in) != 1)
+                continue;
+            if (code_unit->code[1] >> 6 != 0x2) {
+                code_unit->code[0] = code_unit->code[1];
+                continue;
+            }
+            if (fread(&(code_unit->code[2]), 1, 1, in) != 1)
+                continue;
+            if (code_unit->code[2] >> 6 != 0x2) {
+                code_unit->code[0] = code_unit->code[2];
+                continue;
+            }
+            code_unit->length = 3;
+            return 0;
+        } else if (code_unit->code[0] >> 3 == 0x1e) {
+            if (fread(&(code_unit->code[1]), 1, 1, in) != 1) {
+                continue;
+            }
+            if (code_unit->code[1] >> 6 != 0x2) {
+                code_unit->code[0] = code_unit->code[1];
+                continue;
+            }
+            if (fread(&(code_unit->code[2]), 1, 1, in) != 1) {
+                continue;
+            }
+            if (code_unit->code[2] >> 6 != 0x2) {
+                code_unit->code[0] = code_unit->code[2];
+                continue;
+            }
+            if (fread(&(code_unit->code[3]), 1, 1, in) != 1)
+                continue;
+            if (code_unit->code[3] >> 6 != 0x2) {
+                code_unit->code[0] = code_unit->code[3];
+                continue;
+            }
+            code_unit->length = 4;
+
+            return 0;
+        }
+        fread(code_unit->code, 1, 1, in);
+    }
 }
